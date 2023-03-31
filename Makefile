d9a1c86c564c15568e7b47144f5fb9964a303ffe
John Bryant <john.r.bryant@gmail.com>
2023-04-01 07:27:49 +1100

    finished draft of code for hmd_transform

--- Makefile
+++ Makefile
@@ -1,19 +1,18 @@
 
-EPSILON = 0.0001
 N_PC = 10
 
 .PHONY: all
-all: out/svd_mortality_20221129.csv
+all: out/hmd_transform_20221129.rds
 
 ## 2022-11-29 -----------------------------------------------------------------
 
 out/lt_20221129.rds: src/lt.R \
   data/hmd_statistics_20221129.zip
-	Rscript $^ --version=20221129
+	Rscript $^ $@
 
-out/svd_mortality_num_20221129.rds: src/svd_mortality_num.R \
+out/hmd_transform_20221129.rds: src/hmd_transform.R \
   out/lt_20221129.rds
-	Rscript $^ --epsilon=$(EPSILON) --n_pc=$(N_PC) --version=20221129
+	Rscript $^ $@ --n_pc=$(N_PC)
 
 
 ## Clean
--- src/lt.R
+++ src/lt.R
@@ -5,7 +5,7 @@ library(readr)
 library(purrr)
 
 cmd_assign(zipfile = "data/hmd_statistics_20221129.zip",
-           version = "20221129")
+           .out = "out/lt_20221129.rds")
 
 dirs <- c("lt_both/bltper_1x1",
           "lt_both/bltper_5x1",
@@ -21,7 +21,7 @@ data_dir <- file.path(tmp_dir, sub("\\.zip$", "", basename(zipfile)))
 get_data <- function(dir) {
     country_code <- sub("([A-Z]+)\\..*", "\\1", list.files(dir))
     sex <- sub(".*lt_([a-z]+).*", "\\1", dir)
-    plan <- sub(".*ltper_([1x5]+).*", "\\1", dir)
+    type_age <- sub(".*ltper_([1x5]+).*", "\\1", dir)
     files <- list.files(dir, full.names = TRUE)
     ans <- map(files,
                read_table,
@@ -31,7 +31,7 @@ get_data <- function(dir) {
     names(ans) <- country_code
     ans <- bind_rows(ans, .id = "country")
     ans$sex <- sex
-    ans$plan <- plan
+    ans$type_age <- type_age
     ans
 }
 
@@ -45,14 +45,15 @@ unlink(tmp_dir, recursive = TRUE)
 
 
 has_missing <- lt %>%
-    group_by(country, time, plan) %>%
-    summarise(has_missing = anyNA(dx) || anyNA(Lx)) %>%
-    ungroup()
+    group_by(country, time, type_age) %>%
+    summarise(has_missing = anyNA(dx) || anyNA(Lx), .groups = "drop")
 
-lt <- inner_join(lt, has_missing, by = c("country", "time", "plan")) %>%
+lt <- inner_join(lt, has_missing, by = c("country", "time", "type_age")) %>%
     filter(!has_missing) %>%
-    select(-has_missing)
+    select(-has_missing) %>%
+    mutate(sex = case_when(sex == "female" ~ "Female",
+                           sex == "male" ~ "Male",
+                           sex == "both" ~ "Both"))
 
-
-saveRDS(lt, file = sprintf("out/lt_%s.rds", version))
+saveRDS(lt, file = .out)
 
